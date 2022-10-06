c71c41060def7408d4ff9966078caba80ffebb8f
PSPT Mac <pspt.planb@gmail.com>
2022-10-06 11:09:46 +0700

    tst

--- lib/flutter_pkg.dart
+++ lib/flutter_pkg.dart
@@ -1,7 +1,30 @@
 library flutter_pkg;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class CustomButton extends StatelessWidget {
+  // ignore: prefer_typing_uninitialized_variables
+  var onPressed;
+  final Widget child;
+  // ignore: prefer_typing_uninitialized_variables
+  var style;
+  CustomButton({Key? key, @required this.onPressed, required this.child, this.style})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: onPressed,
+      style: TextButton.styleFrom(
+        foregroundColor: Colors.white, padding: const EdgeInsets.all(16.0),
+        backgroundColor: Colors.blue,
+        elevation: 9.0,
+        textStyle: const TextStyle(
+          fontSize: 20,
+        ),
+      ),
+      child: child
+    );
+  }
 }
